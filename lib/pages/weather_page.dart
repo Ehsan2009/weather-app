3b8d4a5865c79a9efe7b66fec1632b55b051977e
Ehsan Javdan <ehsanjavdan77@gmail.com>
2024-10-09 12:23:03 +0400

    added the lottie animations and showing data on the screen with some styles

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/weather.dart';
 import 'package:weather_app/services/weather_service.dart';
 
@@ -26,22 +28,77 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sun.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/storm.json';
+      case 'clear':
+        return 'assets/sun.json';
+      default:
+        return 'assets/sun.json';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     fetchWeather();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_weather?.cityName ?? 'loading city...'),
-            Text('${_weather?.temperature.round()}C'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                color: Colors.grey[400],
+              ),
+              Text(
+                _weather?.cityName ?? 'loading city...',
+                style: GoogleFonts.abel(
+                  textStyle: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+              const Spacer(),
+              _weather == null
+                  ? const CircularProgressIndicator()
+                  : Text(
+                      '${_weather?.temperature.round()}°C',
+                      style: GoogleFonts.abel(
+                        textStyle: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+            ],
+          ),
         ),
       ),
     );
